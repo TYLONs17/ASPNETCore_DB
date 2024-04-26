bb6070598722a4f3cf274e07e4f82a627faf34f2
TYLONs -DES <tylonsroar@gmail.com>
2024-04-26 14:33:02 +0200

    26 April changes , UI adjustments , still TODO: fix about carousel & do testing for submision.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -19,3 +19,79 @@ function updateRecipient() { // Called when the form is submitted
     return true // Allows the form submission to proceed
 }
 
+
+
+//
+
+//document.addEventListener('DOMContentLoaded', function () {
+//    // Get the carousel element
+//    var carousel = document.getElementById('carouselExampleControls')
+
+//    // Get the carousel items
+//    var carouselItems = carousel.querySelectorAll('.carousel-item')
+
+//    // Set the initial active item index
+//    var activeIndex = 0
+
+//    // Function to show the next slide
+//    function showNextSlide() {
+//        // Hide the current active item
+//        carouselItems[activeIndex].classList.remove('active')
+
+//        // Increment the active index or loop back to the start if at the end
+//        activeIndex = (activeIndex + 1) % carouselItems.length
+
+//        // Show the next item
+//        carouselItems[activeIndex].classList.add('active')
+//    }
+
+//    // Function to show the previous slide
+//    function showPrevSlide() {
+//        // Hide the current active item
+//        carouselItems[activeIndex].classList.remove('active')
+
+//        // Decrement the active index or loop back to the end if at the start
+//        activeIndex = (activeIndex - 1 + carouselItems.length) % carouselItems.length
+
+//        // Show the previous item
+//        carouselItems[activeIndex].classList.add('active')
+//    }
+
+//    // Add event listeners to the next and previous controls
+//    var nextControl = carousel.querySelector('.carousel-control-next')
+//    var prevControl = carousel.querySelector('.carousel-control-prev')
+
+//    nextControl.addEventListener('click', function (event) {
+//        event.preventDefault()
+//        showNextSlide()
+//    })
+
+//    prevControl.addEventListener('click', function (event) {
+//        event.preventDefault()
+//        showPrevSlide()
+//    })
+//})
+
+
+
+//document.addEventListener('DOMContentLoaded', function () {
+//    var carousel = document.getElementById('carouselExampleControls')
+//    var pauseButton = document.getElementById('pausePlayButton')
+
+//    // Add event listener to detect when the carousel is paused
+//    carousel.addEventListener('slid.bs.carousel', function (event) {
+//        // Check if the carousel is paused
+//        if (event.detail && event.detail.paused) {
+//            // Change the button icon to play
+//            pauseButton.innerHTML = '<i class="fas fa-play"></i>'
+//        }
+//        //else {
+//        //    // Change the button icon back to pause
+//        //    pauseButton.innerHTML = '<i class="fas fa-pause"></i>'
+//        //}
+//    })
+//})
+
+
+
+//
